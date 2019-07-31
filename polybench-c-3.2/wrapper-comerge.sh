57dcfb353051d38b8df13431defaeb0104277bc6
fsgeek <fsgeek@gatech.edu>
2019-07-31 08:44:29 -0400

    Scripts from July 30 run

---
+++ polybench-c-3.2/wrapper-comerge.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo VMMALLOC_POOL_DIR=$1 VMMALLOC_POOL_SIZE=$2 LD_PRELOAD=libvmmalloc.so.1 $3 >> $4
+VMMALLOC_POOL_DIR=$1 VMMALLOC_POOL_SIZE=$2 LD_PRELOAD=libvmmalloc.so.1 $3 >> $4 2>&1
+
